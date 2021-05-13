899471f26bbbd4486dd7fe046c3aec0d98b186ee
Krasimir Raykov <raykov@weichain.tech>
2021-05-13 15:22:29 +0300

    chore: remove console logs

--- contracts/Portal.sol
+++ contracts/Portal.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./interfaces/IMigrator.sol";
-import "hardhat/console.sol";
 
 contract Portal is ReentrancyGuard {
     using SafeERC20 for IERC20;
